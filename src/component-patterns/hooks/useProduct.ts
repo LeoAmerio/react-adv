a27385f3fd5a8cbba2d6148b0babc5ff689837a5
LeoAmerio14 <leoamerio14@gmail.com>
2024-06-23 03:04:15 -0300

    control props version alt

--- src/component-patterns/hooks/useProduct.ts
+++ src/component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { OnChangeArgs, Product } from '../interfaces/interfaces';
 
 interface useProductProps {
@@ -10,13 +10,7 @@ interface useProductProps {
 export const useProduct = ({ onChange, product, value = 0 }: useProductProps) => {
   const [counter, setCounter] = useState(value);
 
-  const isControlled = useRef( !!onChange );
-
   const incraseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange!({ count: value, product });
-    }
-    
     const newValue = Math.max(counter + value, 0)
     setCounter(newValue);
 
--- src/component-patterns/hooks/useShoppingCart.ts
+++ src/component-patterns/hooks/useShoppingCart.ts
@@ -8,31 +8,16 @@ export const useShoppingCart = () => {
   const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
     // console.log("onProductCountChange: ", counter, product);
     setShopppingCart((prevShoppingCart) => {
-
-      const productInCard: ProducCartProps = prevShoppingCart[product.id] || { ...product, count: 0 };
-
-      if (Math.max(productInCard.count + count, 0) > 0) {
-        productInCard.count += count;
-        return {
-          ...prevShoppingCart,
-          [product.id]: productInCard
-        }
+      if (count === 0) {
+        // delete ({ ...prevShoppingCart } as any)[product.id]; //! (as any) to avoid TS error (delete expects an object, not a type
+        const { [product.id]: toDelete, ...rest } = prevShoppingCart;
+        return rest;
       }
 
-      // Borrar el producto
-      const { [product.id]: toDelete, ...rest } = prevShoppingCart;
-      return rest;
-
-      // if (count === 0) {
-      //   // delete ({ ...prevShoppingCart } as any)[product.id]; //! (as any) to avoid TS error (delete expects an object, not a type
-      //   const { [product.id]: toDelete, ...rest } = prevShoppingCart;
-      //   return rest;
-      // }
-
-      // return {
-      //   ...prevShoppingCart,
-      //   [product.id]: { ...product, count }
-      // }
+      return {
+        ...prevShoppingCart,
+        [product.id]: { ...product, count }
+      }
     });
   }
 
